30363d597edb07e417b5cddf6200e06ae50e6aff
Aya <4587game.aya@gmail.com>
2021-12-24 11:33:21 +0900

    #4

---
+++ setup.bat
@@ -0,0 +1,37 @@
+@echo off
+cd /d %~dp0
+mkdir tmp
+curl -o "tmp\main.zip" "https://codeload.github.com/aya-0p/discord-bot/zip/refs/heads/main"
+call powershell -command "Expand-Archive -Force tmp\main.zip tmp"
+robocopy tmp\discord-bot-main\ %~dp0 /S /E
+rmdir /S /Q tmp
+mkdir saves
+echo {"voice":{"default":1},"replaces":{"text":[],"regex":[]}} > settings.json
+echo # discord token > .env
+echo. >> .env
+echo token= >> .env
+echo. >> .env
+echo. >> .env
+echo # show "debug" log >> .env
+echo. >> .env
+echo debug=false >> .env
+echo. >> .env
+echo. >> .env
+echo # discord server id >> .env
+echo. >> .env
+echo server= >> .env
+echo. >> .env
+echo. >> .env
+echo # VOICEVOX server url and port >> .env
+echo. >> .env
+echo voicevox_url=http://localhost:50021 >> .env
+echo. >> .env
+echo. >> .env
+echo # log everything >> .env
+echo. >> .env
+echo logall=false >> .env
+echo [] > replys.json
+echo. > .log
+echo ### Open '.env' and write token, server id ###
+echo ### Install VOICEVOX ###
+npm install
