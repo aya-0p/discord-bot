214266ee279d96a875d987e8affdddf47db3d937
Aya <4587game.aya@gmail.com>
2022-01-02 18:11:32 +0900

    #6

--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
 @echo off
 mkdir tmp
-curl -o "tmp\main.zip" "https://codeload.github.com/aya-0p/discord-bot/zip/refs/heads/main"
-call powershell -command "Expand-Archive -Force tmp\main.zip tmp"
-robocopy tmp\discord-bot-main\ %~dp0 /S /E
+curl -o "tmp\1.0.2.zip" "https://github.com/aya-0p/discord-bot/archive/refs/tags/v1.0.2.zip"
+call powershell -command "Expand-Archive -Force tmp\1.0.2.zip tmp"
+robocopy tmp\discord-bot-1.0.2\ %~dp0 /S /E
 rmdir /S /Q tmp
 mkdir saves
 echo {"voice":{"default":1},"replaces":{"text":[],"regex":[]}} > jsons\settings.json
