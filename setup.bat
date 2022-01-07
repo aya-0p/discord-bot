e8b54aa8909cbfdcd0814f06af88febe91d8b413
Aya <4587game.aya@gmail.com>
2022-01-07 17:42:37 +0900

    不要な設定の削除

--- setup.bat
+++ setup.bat
@@ -2,34 +2,19 @@
 mkdir tmp
 mkdir jsons
 mkdir musics
-curl -o "tmp\1.0.3.zip" "https://github.com/aya-0p/discord-bot/archive/refs/tags/v1.0.3.zip"
-call powershell -command "Expand-Archive -Force tmp\1.0.3.zip tmp"
-robocopy tmp\discord-bot-1.0.3\ %~dp0 /S /E
-rmdir /S /Q tmp
+
 mkdir saves
 echo {"voice":{"default":1},"replaces":{"text":[],"regex":[]}} > jsons\settings.json
 echo # discord token > .env
-echo. >> .env
 echo token= >> .env
 echo. >> .env
-echo. >> .env
-echo # show "debug" log >> .env
-echo. >> .env
+echo # show debug log >> .env
 echo debug=false >> .env
 echo. >> .env
-echo. >> .env
-echo # discord server id >> .env
-echo. >> .env
-echo server= >> .env
-echo. >> .env
-echo. >> .env
 echo # VOICEVOX server url and port >> .env
-echo. >> .env
 echo voicevox_url=http://localhost:50021 >> .env
 echo. >> .env
-echo. >> .env
 echo # log everything >> .env
-echo. >> .env
 echo logall=false >> .env
 echo [] > jsons\replys.json
 echo. > .log
