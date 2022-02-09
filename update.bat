11afbb018d75499a67e5e29861c5e3b53d6dbd5d
aya-0p <4587game.aya@gmail.com>
2022-02-09 22:32:42 +0900

    内容をプライベートに移すため

--- update.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-mkdir tmp
-curl -o "tmp\1.0.3.zip" "https://github.com/aya-0p/discord-bot/archive/refs/tags/v1.0.3.zip"
-call powershell -command "Expand-Archive -Force tmp\1.0.3.zip tmp"
-robocopy tmp\discord-bot-1.0.3\ %~dp0 /S /E
-rmdir /S /Q tmp
-npm install
